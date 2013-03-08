b3b21b8ea0af1042ffbebe815fe3ff2c74044b4b
btevfik <bt@bu.edu>
2013-03-08 06:06:01 -0500

    initial commit

---
+++ Default.aspx
@@ -0,0 +1,82 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Currency Converter</title>
+    <link rel="stylesheet" type="text/css" href="StyleSheet.css"/>
+     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+      <script>
+          //show the selected currency rate in CurrentRate label
+          $(document).ready(function(){
+              var handle = (function () {
+                 var item = $("#CurrencyBox option:selected");
+                 $("#CurrentRate").text(item.val());
+          });
+          $("#CurrencyBox").change(handle).keyup(handle);
+          });
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="main">
+    <h1>Currency Converter</h1>
+    Target Currency <br />
+    <asp:ListBox ID="CurrencyBox" runat="server" DataSourceID="Sqldatasource1" DataTextField="Country" DataValueField="Rate" Height="143px" Width="290px" OnSelectedIndexChanged="CurrencyBox_SelectedIndexChanged"></asp:ListBox>
+        
+        <br />
+
+        <asp:Button class="button" ID="DeleteButton" runat="server" OnClick="DeleteButton_Click" OnClientClick="if(!confirm('Are you sure?'))return false;"
+ Text="Delete"/>
+        
+        <br /><br />
+
+        <div>
+            Current Rate:
+            <asp:Label ID="CurrentRate" runat="server">N/A</asp:Label>
+            , New Rate:
+             <asp:TextBox ID="Rate" runat="server" Width="40px"></asp:TextBox>
+            <asp:Button class="button" ID="ModifyButton" runat="server" OnClick="ModifyButton_Click" Text="Change" />
+        </div>
+        
+        <br />
+        
+        <div>
+        New Currency:
+        <asp:TextBox ID="NewCurrency" runat="server"></asp:TextBox>
+       , Rate:
+        <asp:TextBox ID="NewRate" runat="server" Width="40px"></asp:TextBox>
+        <asp:Button class="button" ID="AddButton" runat="server" OnClick="AddButton_Click" Text="Add" />
+        </div>
+        
+        <br /> <br />
+        Amount in U.S Dollars <br />
+    <asp:TextBox ID="USD" Width="256" runat="server" /> 
+        <br />
+        <br />
+        <asp:Button class="button" Text="Convert" ID="ConvertButton" Width="124px" OnClick="ConvertButton_Click" runat="server"/>
+        <br />
+        <br />
+    <asp:Label ID="Output" runat="server" />
+         </div>
+          
+            <div class="bottom"><p style="text-align:center">&copy; Baris Tevfik</p></div>
+            
+        <asp:sqldatasource ID="Sqldatasource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" SelectCommand="SELECT * FROM [Table]" DeleteCommand="DELETE FROM [Table] WHERE (Country = @countryName)" InsertCommand="INSERT INTO [Table] (Country, Rate) VALUES (@newCurrency, @newRate)" UpdateCommand="UPDATE [Table] SET Rate = @newRate WHERE (Country = @country)">
+        <DeleteParameters>
+            <asp:ControlParameter ControlID="CurrencyBox" Name="countryName" PropertyName="SelectedItem.Text" />
+        </DeleteParameters>
+        <InsertParameters>
+            <asp:ControlParameter ControlID="NewCurrency" Name="newCurrency" PropertyName="Text" />
+            <asp:ControlParameter ControlID="NewRate" Name="newRate" PropertyName="Text" />
+        </InsertParameters>
+                <UpdateParameters>
+                    <asp:ControlParameter ControlID="Rate" Name="newRate" PropertyName="Text" />
+                    <asp:ControlParameter ControlID="CurrencyBox" Name="country" PropertyName="SelectedItem.Text" />
+                </UpdateParameters>
+        </asp:sqldatasource>
+   
+         </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Diagnostics;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(Object sender, EventArgs e)
+    {
+    }
+
+    /*CONVERT CURRENCY*/
+    public void ConvertButton_Click(Object sender, EventArgs e)
+    {
+        // Perform the conversion and display the results
+        try
+        {
+            decimal dollars = Convert.ToDecimal(USD.Text);
+            if (dollars < 0)
+            {
+                throw new FormatException();
+            }
+            decimal rate = Convert.ToDecimal(CurrencyBox.SelectedValue);
+            decimal amount = dollars * rate;
+            Output.Text = amount.ToString("f2") + " " + CurrencyBox.SelectedItem.Text;
+            Output.ForeColor = System.Drawing.ColorTranslator.FromHtml("#000000");
+
+        }
+        catch (FormatException)
+        {
+            Output.Text = "Error: Invalid input!";
+            Output.ForeColor = System.Drawing.ColorTranslator.FromHtml("#FF0000");
+        }
+        catch (OverflowException)
+        {
+            Output.Text = "Error: Input is too large!";
+            Output.ForeColor = System.Drawing.ColorTranslator.FromHtml("#FF0000");
+        }
+
+        //clear field
+        USD.Text = "";
+    }
+
+    /* DELETE A CURRENCY*/
+    protected void DeleteButton_Click(object sender, EventArgs e)
+    {
+        //delete the selected item from the database
+        if (Sqldatasource1.Delete() <= 0)
+        {
+            Output.Text = "Error: Currency haven't been deleted!";
+            Output.ForeColor = System.Drawing.ColorTranslator.FromHtml("#FF0000");
+        }
+        else
+        {
+            Output.Text = "Currency successfully deleted.";
+            Output.ForeColor = System.Drawing.ColorTranslator.FromHtml("#008000");
+        }
+
+    }
+
+    /* ADD A NEW CURRENCY */
+    protected void AddButton_Click(object sender, EventArgs e)
+    {
+        //see if new currency field is empty
+        if (NewCurrency.Text == "")
+        {
+            Output.Text = "Error: Currency cannot be empty!";
+            Output.ForeColor = System.Drawing.ColorTranslator.FromHtml("#FF0000");
+            return;
+        }
+
+        //try to convert new rate, and catch an exception
+        try 
+        {
+            if (Decimal.Parse(NewRate.Text) <= 0)
+            {
+                throw new FormatException();
+            }
+        }
+        catch(FormatException)
+        {
+            Output.Text = "Error: Invalid rate!";
+            Output.ForeColor = System.Drawing.ColorTranslator.FromHtml("#FF0000");
+            //clear the fields
+            NewCurrency.Text = "";
+            NewRate.Text = "";
+            return;
+        }
+
+        //if the currency already exists, do not add it
+        if(CurrencyBox.Items.FindByText(NewCurrency.Text)!=null)
+        {
+            Output.Text = "Error: Currency already exists!";
+            Output.ForeColor = System.Drawing.ColorTranslator.FromHtml("#FF0000");
+            //clear the fields
+            NewCurrency.Text = "";
+            NewRate.Text = "";
+            return;
+        }
+
+        //finally good to insert the new currency to the database
+        Sqldatasource1.Insert();
+
+        Output.Text = "Currency successfully added.";
+        Output.ForeColor = System.Drawing.ColorTranslator.FromHtml("#008000");
+        
+        //clear the fields
+        NewCurrency.Text = "";
+        NewRate.Text = "";
+    }
+
+    /* MODIFY THE CURRENT RATE */
+    protected void ModifyButton_Click(object sender, EventArgs e)
+    {
+        //try to convert new rate, and catch an exception
+        try
+        {
+            if (Decimal.Parse(Rate.Text) <= 0)
+            {
+                throw new FormatException();
+            }
+        }
+        catch (FormatException)
+        {
+            Output.Text = "Error: Invalid rate!";
+            Output.ForeColor = System.Drawing.ColorTranslator.FromHtml("#FF0000");
+            //clear the field
+            Rate.Text = "";
+            return;
+        }
+
+        //update the rate in database
+        if (Sqldatasource1.Update() > 0)
+        {
+            CurrentRate.Text = Decimal.Parse(Rate.Text).ToString("f4"); //show new rate
+            Output.Text = "Rate successfully updated.";
+            Output.ForeColor = System.Drawing.ColorTranslator.FromHtml("#008000");
+        }
+        else
+        {
+            Output.Text = "Error: Currency haven't been updated!";
+            Output.ForeColor = System.Drawing.ColorTranslator.FromHtml("#FF0000");
+        }
+        //clear the field
+        Rate.Text = "";
+    }
+
+    protected void CurrencyBox_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        //show rate on postback
+        CurrentRate.Text = CurrencyBox.SelectedValue;
+    }
+}
